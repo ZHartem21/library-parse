1f732dd6deddd1bd6b8030a04e7ced555b66eeb0
ZHartem21 <zhartem21@gmail.com>
2023-01-26 16:26:20 +0300

    Download book covers

--- main.py
+++ main.py
@@ -20,27 +20,58 @@ def get_book_info_by_id(id):
     return title.strip(), author.strip()
 
 
-def download_books_in_folder(book_ids, folder):
+def get_book_image_url_by_id(id):
+    url = f'https://tululu.org/b{id}'
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        check_for_redirect(response)
+        soup = BeautifulSoup(response.text, 'lxml')
+        image_path = soup.find('div', class_='bookimage').find('img')['src']
+        if image_path == '/images/nopic.gif':
+            return None
+        image_link = urljoin('https://tululu.org/', image_path)
+        return image_link
+    except requests.HTTPError:
+        return None
+
+
+def download_image(id, folder):
+    os.makedirs(folder, exist_ok=True)
+    image_url = get_book_image_url_by_id(id)
+    if image_url:
+        response = requests.get(image_url)
+        response.raise_for_status()
+        check_for_redirect(response)
+        with open(sanitize_filepath(os.path.join(folder, sanitize_filename(f'{id}.jpg'))), 'wb') as file:
+            file.write(response.content)
+
+
+def download_text(id, folder):
     url = "https://tululu.org/txt.php"
     params = {}
-    os.makedirs(folder, exist_ok=True)
+    try:
+        os.makedirs(folder, exist_ok=True)
+        params['id'] = id
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        check_for_redirect(response)
+        title, author = get_book_info_by_id(id)
+        with open(sanitize_filepath(os.path.join(folder, sanitize_filename(f'{id}. {title}.txt'))), 'wb') as file:
+            file.write(response.content)
+    except requests.HTTPError:
+        print(f'id {id} - неверный')
+
+
+def download_books_in_folder(book_ids, book_folder, image_folder):
     for id in book_ids:
-        try:
-            params['id'] = id
-            response = requests.get(url, params=params)
-            response.raise_for_status()
-            check_for_redirect(response)
-            title, author = get_book_info_by_id(id)
-            with open(sanitize_filepath(os.path.join(folder, sanitize_filename(f'{id}. {title}.txt'))), 'wb') as file:
-                file.write(response.content)
-        except requests.HTTPError:
-            print(f'id {id} - неверный')
+        download_text(id, book_folder)
+        download_image(id, image_folder)
 
 
 def main():
     book_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    # download_books_in_folder(book_ids, 'books')
-
+    download_books_in_folder(book_ids, 'books', 'images')
 
 
 if __name__ == '__main__':
