89a7cd596331e3c5f3db20807062697b9e6e7eb0
ZHartem21 <zhartem21@gmail.com>
2023-01-26 15:29:02 +0300

    Download books with titles

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
 import os
+from urllib.parse import urljoin
 
 import requests
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename, sanitize_filepath
 
 
-def download_tutulu_books_in_folder(book_ids, folder):
+def check_for_redirect(response):
+    if response.url == 'https://tululu.org/':
+        raise requests.HTTPError
+
+
+def get_book_info_by_id(id):
+    url = f'https://tululu.org/b{id}'
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    title, author = soup.find('td', class_='ow_px_td').find('h1').text.split('::')
+    return title.strip(), author.strip()
+
+
+def download_books_in_folder(book_ids, folder):
     url = "https://tululu.org/txt.php"
     params = {}
     os.makedirs(folder, exist_ok=True)
     for id in book_ids:
-        params['id'] = id
-        response = requests.get(url, params=params)
-        response.raise_for_status()
-        with open(os.path.join(folder, f'id{id}.txt'), 'wb') as file:
-            file.write(response.content)
+        try:
+            params['id'] = id
+            response = requests.get(url, params=params)
+            response.raise_for_status()
+            check_for_redirect(response)
+            title, author = get_book_info_by_id(id)
+            with open(sanitize_filepath(os.path.join(folder, sanitize_filename(f'{id}. {title}.txt'))), 'wb') as file:
+                file.write(response.content)
+        except requests.HTTPError:
+            print(f'id {id} - неверный')
 
 
 def main():
     book_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    download_tutulu_books_in_folder(book_ids, 'books')
+    # download_books_in_folder(book_ids, 'books')
+
 
 
 if __name__ == '__main__':
